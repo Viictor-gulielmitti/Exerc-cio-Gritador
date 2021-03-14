d622f6ed5242e674582d81adae2dcdf13c53c975
Viictor-gulielmitti <79719548+Viictor-gulielmitti@users.noreply.github.com>
2021-03-14 18:16:09 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String texto;
+
+            Console.WriteLine("Digite alguma coisa:");
+            texto = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Gritador:");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write($"{texto}".ToUpper());
+        }
+    }
+}
